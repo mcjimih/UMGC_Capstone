6df5cfbd139248d1905773abc77e0c4bae520855
mcjimih <151495329+mcjimih@users.noreply.github.com>
2023-11-29 22:59:10 -0600

    Update script.js
    Added function for opening alerts page

--- script.js
+++ script.js
@@ -1,175 +1,195 @@
 // Check if the script is running in a browser environment
 if (typeof window !== 'undefined') {
-const cityInput = document.querySelector(".city-input");
-const searchButton = document.querySelector(".search-btn");
-const locationButton = document.querySelector(".location-btn");
-const currentWeatherDiv = document.querySelector(".current-weather");
-const weatherCardsDiv = document.querySelector(".weather-cards");
-const switchElement = document.getElementById("toggleTemperature");
-const callImperial = "units=imperial"
-const callMetric = "units=metric"
+    const cityInput = document.querySelector(".city-input");
+    const searchButton = document.querySelector(".search-btn");
+    const locationButton = document.querySelector(".location-btn");
+    const currentWeatherDiv = document.querySelector(".current-weather");
+    const weatherCardsDiv = document.querySelector(".weather-cards");
+    const switchElement = document.getElementById("toggleTemperature");
+    const alertPage = document.querySelector(".alerts-btn");
+    const callImperial = "units=imperial"
+    const callMetric = "units=metric"
 
-let currentWeatherData = null;
-let currentUnit = callMetric;
+    let currentWeatherData = null;
+    let currentUnit = callMetric;
 
-const fahrenheitSign = "°F";
-const celsiusSign = "°C"
-let currentTempUnit = celsiusSign;
+    const fahrenheitSign = "°F";
+    const celsiusSign = "°C"
+    let currentTempUnit = celsiusSign;
 
-const imperialWind = "mph";
-const metricWind = "m/s"
-let currentWindUnit = metricWind;
+    const imperialWind = "mph";
+    const metricWind = "m/s"
+    let currentWindUnit = metricWind;
 
 
-const API_KEY = "6b75e1f27bd464fbc659aabb6312388f"; // OpenWeatherMap API key
+    const API_KEY = "6b75e1f27bd464fbc659aabb6312388f"; // OpenWeatherMap API key
 
-const createWeatherCard = (cityName, weatherItem, index) => {
-    if (index === 0) {
-        return " ";
-    } else {
-        return `<li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                    <h4>Temp: ${(weatherItem.main.temp).toFixed(2)}${currentTempUnit}</h4>
-                    <h4>Wind: ${weatherItem.wind.speed}${currentWindUnit}</h4>
-                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-                </li>`;
-    }
-}
-
-const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&${currentUnit}`;
-    
-    return new Promise((resolve, reject) => {
-        fetch(WEATHER_API_URL)
-            .then(res => res.json())
-            .then(data => {
-                currentWeatherData = data;
-                const uniqueForecastDays = [];
-
-                const sevenDaysForecast = data.list.filter(forecast => {
-                    const forecastDate = new Date(forecast.dt_txt).getDate();
-                    if (!uniqueForecastDays.includes(forecastDate)) {
-                        return uniqueForecastDays.push(forecastDate);
-                    }
-                });
-
-                // Clears old data
-                cityInput.value = "";
-                currentWeatherDiv.innerHTML = "";
-                weatherCardsDiv.innerHTML = "";
-
-                sevenDaysForecast.forEach((weatherItem, index) => {
-                   weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
-                });
-
-                resolve(); // Resolve the promise when data is fetched
-            })
-            .catch(error => {
-                reject(error); // Reject the promise if there's an error
-            });
-    });
-}
-
-const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim(); // deletes trailing spaces after user input
-    if (!cityName) return;
-    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
-
-    fetch(GEOCODING_API_URL)
-        .then(res => res.json())
-        .then(data => {
-            if (!data.length) return alert(`No coordinates found for ${cityName}`);
-            const { name, lat, lon } = data[0];
-            
-            document.getElementById('cityName').textContent = name;
-            getWeatherDetails(name, lat, lon)
-                .then(() => {
-                    const currentDate = currentWeatherData.list[0].dt_txt.split(" ")[0];
-                    document.getElementById('currentDate').textContent = currentDate;
-                    setCurrentWeatherCard();
-                })
-                .catch(error => {
-                    alert(`An error occurred while fetching the weather details: ${error.message}`);
-                });
-        })
-        .catch(() => {
-            alert("An error occurred while fetching the coordinates!");
+    const createWeatherCard = (cityName, weatherItem, index) => {
+        const forecastDate = new Date(weatherItem.dt_txt).toLocaleDateString(undefined, {
+		  weekday: 'short',
+            month: 'short',
+            day: 'numeric'
         });
-}
+    
+        if (index === 0) {
+            return " ";
+        } else {
+            return `<li class="card">
+                        <h3>${forecastDate}</h3>
+                        <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+                        <h4>Temp: ${(weatherItem.main.temp).toFixed(2)}${currentTempUnit}</h4>
+                        <h4>Wind: ${weatherItem.wind.speed}${currentWindUnit}</h4>
+                        <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+                    </li>`;
+        }
+    }
 
-const getLocalCoordinates = () => {
-    navigator.geolocation.getCurrentPosition(
-        position => {
-            // Pulls user location and searches for city
-            const { latitude, longitude } = position.coords;
-            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
-            
-            fetch(REVERSE_GEOCODING_URL)
+    const getWeatherDetails = (cityName, lat, lon) => {
+        const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&${currentUnit}`;
+    
+        return new Promise((resolve, reject) => {
+            fetch(WEATHER_API_URL)
                 .then(res => res.json())
                 .then(data => {
-                    const { name } = data[0];
-                
-                    document.getElementById('cityName').textContent = name;
-                    getWeatherDetails(name, latitude, longitude)
-                        .then(() => {
-                            const currentDate = currentWeatherData.list[0].dt_txt.split(" ")[0];
-                            document.getElementById('currentDate').textContent = currentDate;
-                            setCurrentWeatherCard();
-                        })
-                        .catch(error => {
-                            alert(`An error occurred while fetching the weather details: ${error.message}`);
-                        });
+                    currentWeatherData = data;
+                    const uniqueForecastDays = [];
+
+                    const sevenDaysForecast = data.list.filter(forecast => {
+                    const forecastDate = new Date(forecast.dt_txt).getDate();
+                        if (!uniqueForecastDays.includes(forecastDate)) {
+                            return uniqueForecastDays.push(forecastDate);
+                        }
+                    });
+
+                    // Clears old data
+                    cityInput.value = "";
+                    weatherCardsDiv.innerHTML = "";
+
+                    sevenDaysForecast.forEach((weatherItem, index) => {
+                        weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                    });
+
+                    resolve(); // Resolve the promise when data is fetched
                 })
-                .catch(() => {
-                    alert("An error occurred while fetching the city!");
+                .catch(error => {
+                    reject(error); // Reject the promise if there's an error
                 });
-        },
-        error => {
-            if (error.code === error.PERMISSION_DENIED) {
-                alert("Permission was denied. Please reset location permissions and retry.");
+        });
+    }
+
+    const getCityCoordinates = () => {
+        const cityName = cityInput.value.trim(); // deletes trailing spaces after user input
+        if (!cityName) return;
+        const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+        fetch(GEOCODING_API_URL)
+            .then(res => res.json())
+            .then(data => {
+                if (!data.length) return alert(`No coordinates found for ${cityName}`);
+                const { name, lat, lon } = data[0];
+            
+                document.getElementById('cityName').textContent = name;
+                getWeatherDetails(name, lat, lon)
+                    .then(() => {
+                        const currentDate = currentWeatherData.list[0].dt_txt.split(" ")[0];
+                        document.getElementById('currentDate').textContent = currentDate;
+                        setCurrentWeatherCard();
+                    })
+                    .catch(error => {
+                        alert(`An error occurred while fetching the weather details: ${error.message}`);
+                    });
+            })
+            .catch(() => {
+                alert("An error occurred while fetching the coordinates!");
+            });
+    }
+
+    const getLocalCoordinates = () => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                // Pulls user location and searches for city
+                const { latitude, longitude } = position.coords;
+                const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
+            
+                fetch(REVERSE_GEOCODING_URL)
+                    .then(res => res.json())
+                    .then(data => {
+                        const { name } = data[0];
+                
+                        document.getElementById('cityName').textContent = name;
+                        getWeatherDetails(name, latitude, longitude)
+                            .then(() => {
+                                const currentDate = currentWeatherData.list[0].dt_txt.split(" ")[0];
+                                document.getElementById('currentDate').textContent = currentDate;
+                                setCurrentWeatherCard();
+                            })
+                            .catch(error => {
+                                alert(`An error occurred while fetching the weather details: ${error.message}`);
+                            });
+                    })
+                    .catch(() => {
+                        alert("An error occurred while fetching the city!");
+                    });
+            },
+            error => {
+                if (error.code === error.PERMISSION_DENIED) {
+                    alert("Permission was denied. Please reset location permissions and retry.");
+                }
             }
-        }
-    );
-}
+        );
+    }
 
-const setCurrentWeatherCard = () => {
-    const weatherItem = currentWeatherData.list[0]; // Get the current weather item
-
-// Check if the current weather item is available
-    if (weatherItem) {
-        const bottomBlock = document.querySelector(".bottom-block");
+    const setCurrentWeatherCard = () => {
+        const weatherItem = currentWeatherData.list[0]; // Get the current weather item
+        const currentDate = new Date(weatherItem.dt_txt).toLocaleDateString(undefined, {
+		  weekday: 'short',
+		  month: 'short',
+		  day: 'numeric'
+        });
     
-        // Update the HTML content of the bottom block
-        bottomBlock.innerHTML = `
-            <div class="bottom-details">
-                <h4>Temperature: ${(weatherItem.main.temp).toFixed(2)}${currentTempUnit}</h4>
-                <h4>Wind: ${weatherItem.wind.speed}${currentWindUnit}</h4>
-                <h4>Humidity: ${weatherItem.main.humidity}%</h4>
-            </div>
-            <div class="icon">
-                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                <h4>${weatherItem.weather[0].description}</h4>
-            </div>
-        `;
-    }
-};
+        // Check if the current weather item is available
+        if (weatherItem) {
+            const bottomBlock = document.querySelector(".bottom-block");
+    
+            // Update the HTML content of the bottom block
+            bottomBlock.innerHTML = `
+                <div class="bottom-details">
+                    <h4>Temperature: ${(weatherItem.main.temp).toFixed(2)}${currentTempUnit}</h4>
+                    <h4>Wind: ${weatherItem.wind.speed}${currentWindUnit}</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+                </div>
+                <div class="icon">
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h4>${weatherItem.weather[0].description}</h4>
+                </div>
+            `;
+            document.getElementById('currentDate').textContent = currentDate;
+        }
+    };
 
-switchElement.addEventListener("change", () => {
-    if (switchElement.checked) {
-        currentUnit = callImperial;
-        currentTempUnit = fahrenheitSign;
-        currentWindUnit = imperialWind;
-    } else {
-        currentUnit = callMetric;
-        currentTempUnit = celsiusSign;
-        currentWindUnit = metricWind;
+    switchElement.addEventListener("change", () => {
+        if (switchElement.checked) {
+            currentUnit = callImperial;
+            currentTempUnit = fahrenheitSign;
+            currentWindUnit = imperialWind;
+        } else {
+            currentUnit = callMetric;
+            currentTempUnit = celsiusSign;
+            currentWindUnit = metricWind;
+        }
+    
+    });
+    
+    function openAlertsPage() {
+        // Open a new window with alerts.html
+        window.open('alerts.html', '_blank', 'width=600,height=400');
     }
-});
 
-locationButton.addEventListener("click", getLocalCoordinates);
-searchButton.addEventListener("click", getCityCoordinates);
-cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
+
+    alertPage.addEventListener('click', openAlertsPage);
+    locationButton.addEventListener("click", getLocalCoordinates);
+    searchButton.addEventListener("click", getCityCoordinates);
+    cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
 
 } else {
     // Code for the Node.js environment
